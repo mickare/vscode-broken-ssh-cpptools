76fa67dfb46675f1538fff0b8c581f7999732325
mickare <info@mickare.de>
2020-12-19 02:54:47 +0000

    add preLaunchTask problem

--- src/myproject/main.cpp
+++ src/myproject/main.cpp
@@ -1,7 +1,12 @@
 #include "myproject/header.h"
+#include <cstdlib>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
+    std::string test = getenv ("ENV_TEST");
+    std::cout << "ENV_TEST = " << test << std::endl;
+
     myproject::Test a{"Hello World!"};
     a.print();
     return 0;
